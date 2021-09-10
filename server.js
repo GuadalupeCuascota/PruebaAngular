2fcc0de62a705cb03769d3a4e80f02471fefaa9e
unknown <pgcuascotac@utn.edu.ec>
2021-09-10 16:39:35 -0500

    second commit

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-const exp = require('constants');
+
 const express=require('express');
 const path=require('path');
 
 const app=express();
-app.use(express.static('./dist/pruebaA'))
-app.get('/',(req,res)=>res.sendFile('index.html',{root:'dist/pruebaA/'}),);
+
+app.use(express.static('./dist/pruebaA'));
+
+
+app.get('/*',(req,res)=>res.sendFile('index.html',{root:'dist/pruebaA/'}),);
 app.listen(process.env.PORT||8080)
